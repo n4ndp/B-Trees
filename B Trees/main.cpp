36f86a39754e3734c2b800022a202356ada3a56a
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-12-05 21:54:03 +0000

    Add multiple insertions to B-Tree

--- B Trees/main.cpp
+++ B Trees/main.cpp
@@ -2,9 +2,19 @@
 
 int main() {
     BTree<int> tree(2);
-    for (int i = 1; i <= 14; i++) {
-        tree.insert(i);
-    }
+    tree.insert(4);
+    tree.insert(2);
+    tree.insert(20);
+    tree.insert(10);
+    tree.insert(1);
+    tree.insert(14);
+    tree.insert(7);
+    tree.insert(11);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    
     tree.inorder();
+
     return 0;
 }
