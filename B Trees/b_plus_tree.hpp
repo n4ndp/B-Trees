b78e334d8586f25e860239d4951550c235d2c8ae
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-12-07 20:29:32 +0000

    Refactor BPlusTree insert function

--- B Trees/b_plus_tree.hpp
+++ B Trees/b_plus_tree.hpp
@@ -12,72 +12,110 @@
 - Un valor en un nodo interno es el mayor valor del subárbol izquierdo.
 */
 
-template <typename TK>
+template <typename TK, typename TV>
 class BPlusTree {
 private:
     struct Node {
-        int min_degree;
-        int num_keys;
-        bool is_leaf;
+        int min_degree; // Minimum degree (defines the range for number of keys)
+        int num_keys; // Current number of keys
+        bool is_leaf; // Is true when node is leaf. Otherwise false
 
+        // pair<key, value>
+        std::unique_ptr<std::pair<TK, TV>[]> keys;
+        std::unique_ptr<std::shared_ptr<Node>[]> children;
         std::shared_ptr<Node> parent;
-        std::unique_ptr<TK[]> keys;
-        std::unique_ptr<std::shared_ptr<Node>[]> children; // for leaf nodes, children[0] is the next leaf node
 
-        Node(bool is_leaf, int min_degree) : is_leaf(is_leaf), min_degree(min_degree), num_keys(0) {
-            parent = nullptr;
-            keys = std::make_unique<TK[]>(2 * min_degree - 1);
-            children = std::make_unique<std::shared_ptr<Node>[]>(2 * min_degree);
+        Node(bool is_leaf, int min_degree, std::shared_ptr<Node> parent) : is_leaf(is_leaf), min_degree(min_degree), num_keys(0), parent(parent) {
+            keys = std::make_unique<std::pair<TK, TV>[]>(2 * min_degree - 1); // Allocate memory for maximum number of possible keys
+            children = std::make_unique<std::shared_ptr<Node>[]>(2 * min_degree); // Allocate memory for maximum number of possible children
         }
-
-        bool search(TK key) {
-            int i = 0;
-            while (i < num_keys && key > keys[i]) { // find the first key that is greater than or equal to the key
-                i++;
-            }
-            if (keys[i] == key) {
-                return true; // found
-            }
-            if (is_leaf) {
-                return false; // not found
-            }
-            return children[i]->search(key); // recursively search the subtree
-        }
-
-        void range_search(TK start, TK end, std::tuple<TK>& result);
-
-
-    }
+    };
 
     std::shared_ptr<Node> root;
     int min_degree;
 
-public:
+public: // public functions
+
     BPlusTree(int min_degree) : min_degree(min_degree) {
-        root = std::make_shared<Node>(true, min_degree);
+        root = nullptr;
     }
 
     bool search(TK key) {
-        return (root != nullptr) ? root->search(key) : false;
+        return search(root, key) != nullptr;
     }
 
-    std::tuple<TK> range_search(TK start, TK end) {
-        std::tuple<TK> result;
-        root->range_search(start, end, result);
-        return result;
-    }
-
-    void insert(TK key) {
-        if (root == nullptr) {
-            root = std::make_shared<Node>(true, min_degree);
-            root->keys[0] = key;
+    void insert(TK key, TV value) {
+        if (root == nullptr) { // Tree is empty
+            root = std::make_shared<Node>(true, min_degree, nullptr);
+            root->keys[0] = std::make_pair(key, value);
             root->num_keys = 1;
-        } else {
-            if (root->num_keys == 2 * min_degree - 1) {
+        } else { // Tree is not empty
+            std::shared_ptr<Node> node_to_insert = search(root, key);
+
+            if (node_to_insert->num_keys < 2 * min_degree - 1) { // Leaf node is not full
+                insert_non_full(node_to_insert, key, value);
+
+                // Update the next pointer of the leaf node
+                node_to_insert->children[node_to_insert->num_keys] = node_to_insert->children[node_to_insert->num_keys - 1];
+                node_to_insert->children[node_to_insert->num_keys - 1] = nullptr;
+            } else { // Leaf node is full
+                std::shared_ptr<Node> new_node = std::make_shared<Node>(true, min_degree, node_to_insert->parent);
+
                 
-            } else {
+
+                /*// Update next pointer of the leaf new node
+                new_node->children[min_degree - 1] = node_to_insert->children[node_to_insert->num_keys];
+
+                // Copy keys and values to new node
+                for (int i = 0; i < min_degree - 1; ++i) {
+                    new_node->keys[i] = node_to_insert->keys[i + min_degree];
+                    new_node->num_keys++;
+                    node_to_insert->num_keys--;
+                }
+
+                // Update next pointer of the leaf node
+                node_to_insert->children[node_to_insert->num_keys] = new_node;
+
+                // Update parent pointers of the new node's children*/
                 
             }
         }
+    }
+
+private: // helper functions
+    int find_key(std::shared_ptr<Node> node, TK key) {
+        int index = 0;
+        while (index < node->num_keys && node->keys[index].first < key) { // Find the first key greater than or equal to k
+            ++index;
+        }
+        return index;
+    }
+
+    std::shared_ptr<Node> search(std::shared_ptr<Node> node, TK key) {
+        int index = find_key(node, key);
+
+        if (index < node->num_keys && node->keys[index].first == key) {
+            if (node->is_leaf) { // Node is leaf and contains key
+                return node;
+            } else {
+                return search(node->children[index + 1], key);
+            }
+        } else if (node->is_leaf) { // Node is leaf and does not contain key
+            return nullptr;
+        } else {
+            return search(node->children[index], key);
+        }
+    }
+
+    void insert_non_full(std::shared_ptr<Node> node, TK key, TV value) {
+        int index = node->num_keys - 1;
+
+        while (index >= 0 && node->keys[index].first > key) {
+            node->keys[index + 1] = node->keys[index];
+            --index;
+        }
+
+        node->keys[index + 1] = std::make_pair(key, value);
+        ++node->num_keys;
     }
 };
